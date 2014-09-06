7ae83b064e68588547c861968a42aba68469db94
Luis Fortes <fortesl@yahoo.com>
2014-09-05 21:16:42 -0500

    template for Javascript files - initial commit

---
+++ src/js/jsFileTemplate.js
@@ -0,0 +1,14 @@
+/**
+ * Created by fortesl on 9/5/2014.
+ *
+ * all javascript should be included in a function
+ * no global variables need be created.
+ */
+
+(function () {
+    'use strict';
+
+    //TODO your code goes here
+
+
+})();
