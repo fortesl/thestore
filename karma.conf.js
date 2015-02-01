581defb9a6ee5a634f2adbf22e0f859a345a5e76
fortesl <fortesl@yahoo.com>
2015-01-31 22:23:34 -0600

    added travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -85,7 +85,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'],
 
 
     // Continuous Integration mode
