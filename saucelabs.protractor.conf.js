94c22b659796096144eb977fb3386fc96e8d6b6d
fortesl <fortesl@yahoo.com>
2015-02-01 14:04:45 -0600

    added travis ci

--- saucelabs.protractor.conf.js
+++ saucelabs.protractor.conf.js
@@ -1,34 +1,38 @@
+/**
+ * Name: saucelabs.protractor.conf.js
+ * Created by lfortes on 2/1/2015.
+ */
+
 exports.config = {
+    // The address of a running selenium server.
+//    seleniumAddress: 'http://localhost:4444/wd/hub',
 
     // The address where our server under test is running
     baseUrl: 'http://localhost:9000/',
 
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
         'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
         'name': 'thestore'
     },
 
     framework: 'jasmine',
 
-    seleniumServerJar: './tests/e2e/utils/selenium-java/selenium-server-standalone-2.44.0.jar',
-    chromeDriver: './tests/e2e/utils/selenium-java/chromedriver.exe',
-
     // Spec patterns are relative to the location of the
     // spec file. They may include glob patterns.
     specs: [
-        //'tests/e2e/core-module/*spec.js',
-        //'tests/e2e/product-module/*spec.js',
-        //'tests/e2e/product-module/*specs.js',
-//        'tests/e2e/user-module/sign-up-spec.js',
-//        'tests/e2e/user-module/sign-in-spec.js'
         'tests/e2e/**/*spec.js'
     ],
 
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true, // Use colors in the command line report.
-        silent: true    //remove protractor dot reporter
+        silent: true
     },
 
     onPrepare: function() {
@@ -37,6 +41,5 @@ exports.config = {
             displayStacktrace: true,
             displaySpecDuration: true
         })); //add jasmine spec reporter
-        browser.driver.manage().window().maximize();
     }
 };
